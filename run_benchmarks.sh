8b497cbb73d4a992c40f372b568c1930185be741
Eitan Sharabi <eitansha@amazon.com>
2025-09-20 09:56:13 +0000

    Try again to fix run_benchmarks script to have a better summary table

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -25,51 +25,44 @@ key_sizes=("rand-8" "rand-16" "rand-32")
 
 declare -A results
 
-run_benchmark() {
-    local bench_cmd="$1"
-    local key_size="$2"
-    local total_throughput=0
-    local valid_runs=0
-    
-    echo "Running: $bench_cmd $key_size ($RUNS times)"
-    
-    for ((run=1; run<=RUNS; run++)); do
-        echo -n "  Run $run/$RUNS... "
-        output=$(eval "./benchmark $bench_cmd $key_size" 2>&1)
-        
-        # Extract ops and ms from RESULT line
-        result_line=$(echo "$output" | grep "RESULT:")
-        if [[ $result_line =~ ops=([0-9]+).*ms=([0-9]+) ]]; then
-            ops=${BASH_REMATCH[1]}
-            ms=${BASH_REMATCH[2]}
-            throughput=$(echo "scale=3; $ops * 1000 / $ms / 1000000" | bc -l)
-            total_throughput=$(echo "scale=3; $total_throughput + $throughput" | bc -l)
-            valid_runs=$((valid_runs + 1))
-            echo "${throughput} Mops/s"
-        else
-            echo "FAILED"
-        fi
-    done
-    
-    if [[ $valid_runs -gt 0 ]]; then
-        avg_throughput=$(echo "scale=2; $total_throughput / $valid_runs" | bc -l)
-        echo "  Average: ${avg_throughput} Mops/s"
-        echo "$avg_throughput"
-    else
-        echo "  Average: N/A"
-        echo "N/A"
-    fi
-    echo
-}
-
 for key_size in "${key_sizes[@]}"; do
     echo "========================================" | tee -a $RESULTS_FILE
     echo "Testing with $key_size" | tee -a $RESULTS_FILE
     echo "========================================" | tee -a $RESULTS_FILE
     
     for bench in "${benchmarks[@]}"; do
-        avg_result=$(run_benchmark "$bench" "$key_size")
-        results["${bench}_${key_size}"]="$avg_result"
+        echo "Running: $bench $key_size ($RUNS times)"
+        
+        total_throughput=0
+        valid_runs=0
+        
+        for ((run=1; run<=RUNS; run++)); do
+            echo -n "  Run $run/$RUNS... "
+            output=$(eval "./benchmark $bench $key_size" 2>&1)
+            
+            # Extract ops and ms from RESULT line
+            result_line=$(echo "$output" | grep "RESULT:")
+            if [[ $result_line =~ ops=([0-9]+).*ms=([0-9]+) ]]; then
+                ops=${BASH_REMATCH[1]}
+                ms=${BASH_REMATCH[2]}
+                throughput=$(echo "scale=3; $ops * 1000 / $ms / 1000000" | bc -l)
+                total_throughput=$(echo "scale=3; $total_throughput + $throughput" | bc -l)
+                valid_runs=$((valid_runs + 1))
+                echo "${throughput} Mops/s"
+            else
+                echo "FAILED"
+            fi
+        done
+        
+        if [[ $valid_runs -gt 0 ]]; then
+            avg_throughput=$(echo "scale=2; $total_throughput / $valid_runs" | bc -l)
+            echo "  Average: ${avg_throughput} Mops/s"
+            results["${bench}_${key_size}"]="$avg_throughput"
+        else
+            echo "  Average: N/A"
+            results["${bench}_${key_size}"]="N/A"
+        fi
+        echo
     done
 done
 
